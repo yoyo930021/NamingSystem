61d4c72df55584e2890f6351d35e784a8a393efd
yoyo930021 <yoyo930021@gmail.com>
2015-02-06 15:25:10 +0800

    插入首頁視圖

---
+++ app/views/index.blade.php
@@ -0,0 +1,31 @@
+@extends('start.layout')
+
+@section('contain')
+
+<div class="jumbotron" id="start">
+            <h1>Hello, world!</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <center>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">開始使用</a>
+                </p>
+            </center>
+        </div>
+        <div class="col-md-9">
+            @foreach ($posts->$post )
+            <div class="post">
+                <h3><a href="#">{{{ $post->title }}}</a></h3>
+                <div class="infor">Posted:{{{ $post->timestamp }}}</div>
+                <p>{{{ $post->content }}}</p>
+            </div>
+            @endforreach
+        </div>
+
+        <div class="col-md-3">
+            <li>
+                <a href="#">
+                    <p>管理登入</p>
+                </a>
+            </li>
+        </div>
+
+@stop
---
+++ app/views/start/layout.blade.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <title>雲端出缺席系統</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!--<link rel="stylesheet" href="css/bootstrap-theme.min.css" />-->
+    <link rel=stylesheet href="css/index.css" />
+    <script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container" id="contain">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">雲端出缺席系統</a>
+                </div>
+            </div>
+        </nav>
+
+        @yield('contain')
+
+    </div>
+    <div class="container" id="footer">
+        <div class="col-md-12" id="license">
+            <p style="text-align:right">Auto-Distribute System All rights reserved Daan Computer Research Club</p>
+        </div>
+    </div>
+</body>
+
+</html>
